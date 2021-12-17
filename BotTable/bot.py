656d7a10859c024708328b50c4e27a6a73633c68
Felipe Curcio <felipe_nieto010@hotmail.com>
2021-12-16 21:40:18 -0300

    push to github

--- BotTable/bot.py
+++ BotTable/bot.py
@@ -3,11 +3,14 @@ from botcity.core import DesktopBot
 # Using the Maestro SDK
 # from botcity.maestro import *
 from . import api
+import os
+import pandas as pd
 
 GENRES = api.MOVIEDB_API_GENRES()
 
 def filterResults(object):
     genres = object['genre_ids']
+    
     genres = [item['name'] for item in GENRES if item['id'] in genres]
     genres = ";".join(genres)
 
@@ -23,41 +26,84 @@ class Bot(DesktopBot):
     def action(self, execution=None):
         response = api.MOVIEDB_API_ENDPOINT()
         data = list(map(filterResults, response))
+        df = pd.DataFrame(data)
+        df.to_csv(os.path.join(os.path.dirname( __file__ ), 'botCityData.csv'), index=False)
 
         self.browse("https://docs.google.com/spreadsheets/u/0/")
         
+        
         if not self.find( "blank", matching=0.97, waiting_time=10000):
             self.not_found("blank")
-        self.click_relative(70, -71)
+        self.click()
+        
         
         self.wait(4000)
-        if not self.find( "title", matching=0.97, waiting_time=10000):
-            self.not_found("title")
-        self.click()
+        if not self.find( "logo", matching=0.97, waiting_time=10000):
+            self.not_found("logo")
+        self.click_relative(64, 7)
         self.paste("BotCity spreadsheet")
         self.enter()
+
+        # self.wait(4000)
+        if not self.find( "file", matching=0.97, waiting_time=10000):
+            self.not_found("file")
+        self.click()
+
+        if not self.find( "import", matching=0.97, waiting_time=10000):
+            self.not_found("import")
+        self.click()
         
-        length = len(data)
-        # for i in range(length):
-        for i in range(6):
-            if i == 0:
-                for key in data[i].keys():
-                    self.paste(key)
-                    self.type_right()
-            else:
-                for value in data[i].values():
-                    self.paste(value)
-                    self.type_right()
-            self.enter()
-            self.enter()
-            for _ in range(len(data[i].keys())):
-                self.type_left()
-        self.control_a()
-        self.control_a()
-        if not self.find( "fx", matching=0.97, waiting_time=10000):
-            self.not_found("fx")
-        self.double_click_relative(27, 29)
-        self.type_left()
+        if not self.find( "upload", matching=0.97, waiting_time=10000):
+            self.not_found("upload")
+        self.click()
+        
+        if not self.find( "select", matching=0.97, waiting_time=10000):
+            self.not_found("select")
+        self.click()
+        
+        if not self.find( "arrowUp", matching=0.97, waiting_time=10000):
+            self.not_found("arrowUp")
+        self.click_relative(260, 6)
+        
+        dataPath = os.path.abspath(os.path.dirname( __file__ ))
+        self.paste(dataPath)
+        self.enter()
+        
+        if not self.find_text( "botcitydatacsv", threshold=230, waiting_time=10000):
+            self.not_found("botcitydatacsv")
+        self.double_click()
+        
+        if not self.find( "importdata", matching=0.97, waiting_time=10000):
+            self.not_found("importdata")
+        self.click()
+        
+        if not self.find( "opendata", matching=0.97, waiting_time=10000):
+            self.not_found("opendata")
+        self.click()
+        self.wait(3000)
+        
+        # OLD IDEA
+        # length = len(data)
+        # # for i in range(length):
+        # for i in range(7):
+        #     if i == 0:
+        #         for key in data[i].keys():
+        #             self.paste(key)
+        #             self.type_right()
+        #     else:
+        #         for value in data[i].values():
+        #             self.paste(value)
+        #             self.type_right()
+        #     self.enter()
+        #     self.enter()
+        #     for _ in range(len(data[i].keys())):
+        #         self.type_left()
+        # self.control_a()
+        # self.control_a()
+        # if not self.find("fx", matching=0.97, waiting_time=10000):
+        #     self.not_found("fx")
+        # self.double_click_relative(27, 29)
+        # self.type_left()
 
     def not_found(self, label):
         print(f"Element not found: {label}")
@@ -65,6 +111,23 @@ class Bot(DesktopBot):
 
 if __name__ == '__main__':
     Bot.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # BotTable
 
-A bot that fills a spreadsheet
+> A bot that fills a spreadsheet.
 
-## It takes data from The Movie Database API
+[![BotTable Video demonstration](https://img.youtube.com/vi/Lt8UmsEkLpA/0.jpg)](https://www.youtube.com/watch?v=Lt8UmsEkLpA)
 
-[![BotTable Video demonstration](https://img.youtube.com/vi/VIDEO_ID_HERE/0.jpg)](https://www.youtube.com/watch?v=VIDEO_ID_HERE)
+The bot takes from an API the most popular movies as data and turns it into a spreadsheet, so human can read and parse the data.
+
+## The Movie Database
+
+We use its API to get the data
+
+### Pandas
+
+Once we got the data, we filter it and make a ".csv" file from a DataFrame created by the data.
+
+## Bot
+
+The bot will take care of importing the ".csv" file into a google spreadsheet
